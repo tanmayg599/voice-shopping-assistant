87d5892d9711ed216550bbfd7c0dcc89f244891a
Tanmay garg <125557780+tanmayg599@users.noreply.github.com>
2025-08-30 20:23:44 +0530

    Update vite.config.js

--- voice_app/vite.config.js
+++ voice_app/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
-// export default defineConfig({
-//   base: "./",
-//   plugins: [react()],
-// });
+  plugins: [react()],
+  base: "./",  // 👈 important for Vercel
+});
