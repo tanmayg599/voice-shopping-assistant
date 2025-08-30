670ca027b4cf8897fe9c91af78f177c133e05358
Tanmay garg <125557780+tanmayg599@users.noreply.github.com>
2025-08-30 20:20:04 +0530

    Update vite.config.js

--- voice_app/vite.config.js
+++ voice_app/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//   ],
-// })
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
+// export default defineConfig({
+//   base: "./",
+//   plugins: [react()],
+// });
